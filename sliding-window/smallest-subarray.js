ad9d299b22a40cca82071caaa3218acb819492da
anushankar <anushankar@qburst.com>
2022-08-02 20:35:51 +0530

    Smallest sub array

--- sliding-window/smallest-subarray.js
+++ sliding-window/smallest-subarray.js
@@ -2,3 +2,26 @@
 // Input: [2, 1, 5, 2, 3, 2], S=7
 //Output: 2
 //Explanation: The smallest subarray with a sum greater than or equal to ‘7’ is [5, 2].
+
+const array = [2, 1, 5, 2, 3, 2];
+const targetVal = 7;
+
+const findSmallestSubArray = (arr, target) => {
+   let minLength = Infinity;
+   let windowSum = 0;
+   let windowStart = 0; 
+   for(let windowEnd =0 ; windowEnd < arr.length; windowEnd++) {
+    windowSum+= arr[windowEnd];
+    while(windowSum >= target){
+        minLength = Math.min(minLength, windowEnd - windowStart + 1);
+        windowSum -= arr[windowStart]
+        windowStart += 1 // sliding the window
+    }
+   }
+   if (minLength === Infinity) {
+    return 0;
+  }
+  return minLength;
+}
+
+console.log(findSmallestSubArray(array, targetVal));
